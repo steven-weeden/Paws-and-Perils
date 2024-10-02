6a6dc7a52f912427cb4798635b4999bef7e529da
GrantRitter1 <pr01squeak@gmail.com>
2024-10-01 23:29:37 -0500

    Test Case World

---
+++ assets/Nodes/Player/scripts/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+var speed = 75
+
+var player_state 
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction.x == 0 and direction.y == 0:
+		player_state = "idle"
+	elif direction.x != 0 or direction.y != 0:
+		player_state = "walking"
+		
+	velocity = direction * speed
+	move_and_slide()
+	play_anim(direction)
+	
+func play_anim(dir):
+	
+	if player_state == "idle":
+		$AnimatedSprite2D.play("idle")
+	if player_state == "walking":
+		if dir.y == -1:
+			$AnimatedSprite2D.play("n_walk")
+		if dir.y == 1:
+			$AnimatedSprite2D.play("s_walk")
+		if dir.x == -1:
+			$AnimatedSprite2D.play("w_walk")
+		if dir.x == 1:
+			$AnimatedSprite2D.play("e_walk")
+		
+		if dir.x > 0.5 and dir.y < -0.5:
+			$AnimatedSprite2D.play("ne_walk")
+		if dir.x > 0.5 and dir.y > 0.5:
+			$AnimatedSprite2D.play("se_walk")
+		if dir.x < -0.5 and dir.y > 0.5:
+			$AnimatedSprite2D.play("sw_walk")
+		if dir.x < -0.5 and dir.y < -0.5:
+			$AnimatedSprite2D.play("nw_walk")		
