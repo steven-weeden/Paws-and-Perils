c578111b6709a2376e2720ca55d011b3ce73eef3
GrantRitter1 <pr01squeak@gmail.com>
2024-12-09 22:41:32 -0600

    What Happened

--- assets/Nodes/QuestBar/Scripts/heal_check.gd
+++ assets/Nodes/QuestBar/Scripts/heal_check.gd
@@ -6,10 +6,9 @@ var quest: Quest
 var questReady: bool = false
 
 func _on_tutorial_guy_player_rest() -> void:
-	if(questReady):
+	if questReady == true:
 		quest.reached_goal()
 		completeButton.visible = true
 		questReady = false
-	else:
-		completeButton.visible = true
+
 	
